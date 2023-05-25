8b5834ebf30ed19c00c3ac9b7f03f1c35b149db3
giaphule <giaphule.nhc@gmail.com>
2023-05-26 03:37:16 +0700

    Test
    myapp

---
+++ components/DataEmoji.js
@@ -0,0 +1,8 @@
+export const Data = () => [
+    require('../assets/images/emoji1.png'),
+    require('../assets/images/emoji2.png'),
+    require('../assets/images/emoji3.png'),
+    require('../assets/images/emoji4.png'),
+    require('../assets/images/emoji5.png'),
+    require('../assets/images/emoji6.png'),
+]
---
+++ components/IconButton.js
@@ -0,0 +1,22 @@
+import { Pressable, StyleSheet, Text } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+export default function IconButton(props) {
+  return (
+    <Pressable style={styles.iconButton} onPress={props.onPress}>
+      <MaterialIcons name={props.icon} size={24} color="#fff" />
+      <Text style={styles.iconButtonLabel}>{props.label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  iconButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconButtonLabel: {
+    color: '#fff',
+    marginTop: 12,
+  },
+});
